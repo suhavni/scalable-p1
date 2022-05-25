2e6d690eb8c6bb92c0c8c0884472a3427f45112a
suhavni <suhavni@gmail.com>
2022-05-26 02:15:53 +0700

    OMG IT ACTUALLY WORKS! wrote yml files for deployment databases

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,6 @@
 from os import getenv
 
 class Config:
-    """Set Flask configuration from .env file."""
-
-    # General Config
-    # SECRET_KEY = getenv("SECRET_KEY")
-    # FLASK_APP = getenv("FLASK_APP")
-    # FLASK_ENV = getenv("FLASK_ENV")
-
     # Database
     SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = True
