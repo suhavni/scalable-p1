071c8db7e8e6613a84d0498361693d58f8f8c56b
suhavni <suhavni@gmail.com>
2022-05-27 11:31:50 +0700

    more tweaking

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from flask import jsonify, request
 from flask import current_app as app
 from sqlalchemy.exc import DataError
-from werkzeug.contrib.cache import FileSystemCache as Cache
+from werkzeug.contrib.cache import RedisCache as Cache
 
 from . import db
 from .models import Paste
 
 HEADERS = {"title", "content"}
-cache = Cache(cache_dir='cache', threshold=100, default_timeout=1)
+cache = Cache(host='redis', port=6379, password=None, db=0, default_timeout=1)
 
 def jsonify_paste(paste):
     return {
@@ -59,7 +59,7 @@ def get(paste_id):
 def recents():
     res = cache.get('recents')
     if res is None:
-        pastes = Paste.query.order_by(Paste.created_at.desc()).limit(100)
+        pastes = Paste.query.order_by(Paste.id.desc()).limit(100)
         res = [jsonify_paste(paste) for paste in pastes]
         cache.set('recents', res)
     return jsonify(res), 200
